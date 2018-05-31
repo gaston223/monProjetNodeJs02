1896fe8698a497d67893949204f511f68f586eed
gaston223 <g.coulibalyy@gmail.com>
2018-05-31 16:19:49 +0200

    Version avec MongoDB et Mongoose

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
+// On importe les modules dont on a besoin (modules tiers)
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let mongoose = require('mongoose');
 
+// On importe les differents routeurs (les modules à nous (pour le routage))
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let productCreationRouter = require('./routes/product-creation');
 
-
-
+// On créé notre application avec Express
 let app = express();
 
-// view engine setup
+// Definition du moteur de templates vues
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Middleware de base
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//Rendre des assets : CSS/JS/IMG/FONTS
+//Rendre des assets statiques : CSS/JS/IMG/FONTS
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dist', express.static(path.join(__dirname,'node_modules/jquery/dist')));
 app.use('/dist', express.static(path.join(__dirname,'node_modules/bootstrap/dist')));
+
+//Mongoose
+console.log("version de mongoose " +mongoose.version);
+mongoose.connect('mongodb://localhost/food');
+let database = mongoose.connection;
+database.on('error' , (err)=>{
+  console.log("Erruer avec Mongoose");
+  console.log(err);
+});
+
+database.once('open', () => console.log('Connexion réussie avec MongoDB'));
+
+
 
 //Système de routage
 app.use('/', indexRouter);
@@ -36,7 +52,9 @@ app.get('/contact',(req, res) =>{
   res.send('<h1>Bienvenue sur la page de contact</h1>')
 });
 
-
+/** Routes definies à la main
+ * ##todo Exporter les routes
+ */
 app.get('/api/products', (req, res)=>{
   const monObjet = {"name" : "hamac"};
   res.json(monObjet);
@@ -46,12 +64,13 @@ app.get(/(ba)+r+$/, (req, res)=>{
   res.send('URL catched !');
 });
 
+//Au cas ou la route demandée n'existe pas => Gestion de l'erreur 404 : Not Found 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Middleware d'erreurs : executé quand il ya une erreur
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
